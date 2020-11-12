15468752a5ce169afd07b4958829c505f55fc963
Pappy Mekh <63507656+pappymekk@users.noreply.github.com>
2020-11-12 16:38:39 +0000

    While loops deleted - Replaced by time.sleep()
    While loops which caused CPU burning were replaced by the time.sleep() method

--- pomodoro.py
+++ pomodoro.py
@@ -1,7 +1,7 @@
 # Studying during 25 minutes and getting a break of 5 minutes,
 # and then approving either for restudying or finishing the work
 
-import datetime
+import time
 from tkinter import *
 import tkinter.messagebox
 
@@ -46,16 +46,10 @@ print(welcoming())
 
 
 def studying():
-    start = datetime.datetime.now()  # The start time
-    ending = start + datetime.timedelta(minutes=25)  # The expected time for the end
-    while datetime.datetime.now() != ending:  # the current time is not 25 minutes past the start
-        continue
+    time.sleep(1500)
     approval = take_break()
     if approval:  # If there's an approval for the break
-        start_rest = datetime.datetime.now()  # The beginning of the nap time
-        end_rest = start_rest + datetime.timedelta(minutes=5)
-        while datetime.datetime.now() != end_rest:  # the current time is not 5 minutes past the start of the nap
-            continue
+        time.sleep(300)
         study_approval = restudying()
         if study_approval:  # If studying again is approved
             return studying()
